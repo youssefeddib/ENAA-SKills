4288b37e577210fa30f2aa09b7d442d74e5ce522
youssef eddib <eddiby79@gmail.com>
2025-07-04 12:53:10 +0200

    DTO de souscompetence

--- competence-service/src/main/java/com/competenceservice/dto/SousCompetenceDTO.java
+++ competence-service/src/main/java/com/competenceservice/dto/SousCompetenceDTO.java
@@ -1,1 +1,53 @@
+package com.competenceservice.dto;
 
+public class SousCompetenceDTO {
+
+    private Long id;
+    private String nom;
+    private boolean valide;
+
+    // معرف الـ Competence الأب (لربط الـ SousCompetence بالـ Competence)
+    private Long competenceId;
+
+    public SousCompetenceDTO() {
+    }
+
+    public SousCompetenceDTO(Long id, String nom, boolean valide, Long competenceId) {
+        this.id = id;
+        this.nom = nom;
+        this.valide = valide;
+        this.competenceId = competenceId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public boolean isValide() {
+        return valide;
+    }
+
+    public void setValide(boolean valide) {
+        this.valide = valide;
+    }
+
+    public Long getCompetenceId() {
+        return competenceId;
+    }
+
+    public void setCompetenceId(Long competenceId) {
+        this.competenceId = competenceId;
+    }
+}
